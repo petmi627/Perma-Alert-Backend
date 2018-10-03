891fa81757d50edb0ce01dbcd36c850629d73ecc
Mike Peters <mike@petmi627.lu>
2018-10-03 11:16:22 +0200

    Created Headline Resource

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_restful import Api
-from src.Resource.Display import cis, duty
+from src.Resource.Display import cis, duty, headlines
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(cis.CIS, '/cis/<string:location>')
-api.add_resource(duty.Duty, '/cis/<string:location>/duties/<string:engine>')
+api.add_resource(cis.CIS, '/display/cis/<string:location>')
+api.add_resource(duty.Duty, '/display/cis/<string:location>/duties/<string:engine>')
+api.add_resource(headlines.Headlines, '/display/headlines')
 
 
 if __name__ == '__main__':
---
+++ src/Resource/Display/headlines.py
@@ -0,0 +1,5 @@
+from flask_restful import Resource
+
+class Headlines(Resource):
+    def get(self):
+        pass
