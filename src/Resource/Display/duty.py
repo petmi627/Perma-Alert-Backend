f56a5491b36ba57b104d3c9b4ba6d1f52902d12c
Mike Peters <mike@petmi627.lu>
2018-10-31 21:28:33 +0100

    Updated Duty Resource and added CIS Model

--- src/Resource/Display/duty.py
+++ src/Resource/Display/duty.py
@@ -1,9 +1,13 @@
 from flask_restful import Resource, abort
 from src.Models.Display.duty import DutyModel
+from src.Models.Display.cis import CisModel
 
 class Duty(Resource):
     def get(self, location, engine):
         """ Return a Duty List from specified engine """
+        cis = CisModel.get_cis_by_location(location=location)
+        if not cis:
+            abort(404, message="CIS {} doesn't exist".format(location))
 
         duties = DutyModel.get_duty_by_location_engine(engine=engine, location=location)
         duty_list = list(map(lambda x: x.json(), duties))
