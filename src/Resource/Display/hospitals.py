baf126584c9ef7602afa92440c99edc90d8b82da
Mike Peters <mike@petmi627.lu>
2018-11-10 13:32:01 +0100

    Updated Hospital Resource

--- src/Resource/Display/hospitals.py
+++ src/Resource/Display/hospitals.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 from flask_restful import Resource, abort
-import requests, re
+import requests, re, datetime
 
 from src.Models.Display.hospital import HospitalModel
 
@@ -13,11 +13,28 @@ class Hospitals(Resource):
 
        soup = BeautifulSoup(r.text, "html.parser")
 
+       now = datetime.datetime.now()
+       start = datetime.datetime.now().replace(hour=7)
+       end = datetime.datetime.now().replace(hour=19)
+
        hospitalList = []
 
        hospitals = soup.find_all(style=re.compile("background-color: #c6dcc1"))[1:]
        for hospital in hospitals:
-           hospitalModel = HospitalModel.get_hospital_by_alias(hospital.b.string)
-           hospitalList.append(hospitalModel.json())
+
+           hospital_name = hospital.b.string
+
+           hospitals = hospital.find_all("b")
+           if len(hospitals) > 1:
+               for item in hospitals:
+                   if item.next_sibling == "de 7.00 à 19.00 hrs" and start < now and end > now:
+                       hospital_name = item.string
+                       break
+
+                   hospital_name = item.string
+
+           hospitalModel = HospitalModel.get_hospital_by_alias(hospital_name)
+           if hospitalModel:
+                hospitalList.append(hospitalModel.json())
 
        return hospitalList, 200
